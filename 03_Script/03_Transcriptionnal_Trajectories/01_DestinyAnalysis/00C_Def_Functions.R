946c62708ee51427b2f8de76b738d59bd45b66bd
RebuffetLucas <rebuffet@ciml.univ-mrs.fr>
2024-12-13 19:44:23 +0100

    Revert "Initial commit"
    This reverts commit cc31ed7f867faf193664388ce340876c6381bd7b.

--- 03_Script/03_Transcriptionnal_Trajectories/01_DestinyAnalysis/00C_Def_Functions.R
+++
@@ -1,8 +0,0 @@
-# Functions
-exp2col<-function(value,breaks=1000,
-                  cols=viridis::plasma(11, alpha=1, begin = 0, end=1, direction = 1)){
-  colPal <- colorRampPalette(cols)
-  marker.color <- colPal(breaks)[as.numeric(cut(value, breaks = breaks))]
-  return(marker.color)
-}
-
